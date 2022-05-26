346bd1679d91f2bde801011e7838a0b2982c3a76
Pacifist <desmondmorales92@gmail.com>
2022-05-26 19:17:14 +0300

    DOM: attributes, values, selectors

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const ulElem = document.getElementById("ulId")
+const ulAttributes = ulElem.attributes
+const arrAttrValue = []
+const arrAttributes = []
+
+for (let ulAttribute of ulAttributes) {
+    let ulAttrValue = ulAttribute.value
+    arrAttrValue.push(ulAttrValue)
+    arrAttributes.push(ulAttribute)
+}
+
+const ulLastChild = ulElem.querySelector("ul li:last-child")
+ulLastChild.innerHTML = "Привет, меня зовут Alex"
+
+const ulFirstChild = ulElem.querySelector("ul li:first-child")
+ulFirstChild.setAttribute('data-my-name', "Alex")
+
+const ulDeleting = ulElem.removeAttribute("data-dog-tail")
+
+console.log(arrAttrValue)
+console.log(arrAttributes)
+console.log(ulLastChild)
+console.log(ulFirstChild)
