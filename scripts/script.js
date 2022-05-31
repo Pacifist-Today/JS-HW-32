c673fb02d87c76b46edc4ff09e3eebd8fd4bcde2
Pacifist <desmondmorales92@gmail.com>
2022-05-31 18:55:28 +0300

    iife

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,28 @@
-"use strict"
+"use strict";
 
-const ulElem = document.getElementById("ulId")
-const ulAttributes = ulElem.attributes
-const arrAttrValue = []
-const arrAttributes = []
+(function () {
 
-for (let ulAttribute of ulAttributes) {
-    let ulAttrValue = ulAttribute.value
-    arrAttrValue.push(ulAttrValue)
-    arrAttributes.push(ulAttribute)
-}
+    const ulElem = document.getElementById("ulId")
+    const ulAttributes = ulElem.attributes
+    const arrAttrValue = []
+    const arrAttributes = []
 
-const ulLastChild = ulElem.querySelector("ul li:last-child")
-ulLastChild.innerHTML = "Привет, меня зовут Alex"
+    for (let ulAttribute of ulAttributes) {
+        let ulAttrValue = ulAttribute.value
+        arrAttrValue.push(ulAttrValue)
+        arrAttributes.push(ulAttribute)
+    }
 
-const ulFirstChild = ulElem.querySelector("ul li:first-child")
-ulFirstChild.setAttribute('data-my-name', "Alex")
+    const ulLastChild = ulElem.querySelector("ul li:last-child")
+    ulLastChild.innerHTML = "Привет, меня зовут Alex"
 
-const ulDeleting = ulElem.removeAttribute("data-dog-tail")
+    const ulFirstChild = ulElem.querySelector("ul li:first-child")
+    ulFirstChild.setAttribute('data-my-name', "Alex")
 
-console.log(arrAttrValue)
-console.log(arrAttributes)
-console.log(ulLastChild)
-console.log(ulFirstChild)
+    const ulDeleting = ulElem.removeAttribute("data-dog-tail")
+
+    console.log(arrAttrValue)
+    console.log(arrAttributes)
+    console.log(ulLastChild)
+    console.log(ulFirstChild)
+})()
